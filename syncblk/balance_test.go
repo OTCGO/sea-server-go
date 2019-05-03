b11f5533a72f735365222c31511b16ebac59c307
hzxiao <zehao_xiao@163.com>
2019-05-03 10:52:16 +0800

    fix test

--- syncblk/balance_test.go
+++ syncblk/balance_test.go
@@ -1,11 +1,24 @@
 package syncblk
 
 import (
+	"expvar"
+	"github.com/OTCGO/sea-server-go/syncblk/supernode"
 	"github.com/hzxiao/goutil/assert"
 	"testing"
 )
 
+func initSuperNode()  {
+	superNode = &supernode.NodeInfo{
+		FastestNode:     expvar.NewString("fastestNode"),
+		SupportLogNode:  expvar.NewString("supportLogNode"),
+	}
+	superNode.FastestNode.Set(`https://test3.cityofzion.io:443`)
+	superNode.SupportLogNode.Set(`https://test3.cityofzion.io:443`)
+}
+
 func TestRpcGetGlobalBalance(t *testing.T) {
+	initSuperNode()
+
 	b, err := rpcGetGlobalBalance("e13440dccae716e16fc01adb3c96169d2d08d16581cad0ced0b4e193c472eac1",
 		"AGHdThQFJs5kixWuXkgRsbNKz2LrDYDaQB")
 	assert.NoError(t, err)
